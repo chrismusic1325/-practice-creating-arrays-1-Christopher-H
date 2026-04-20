d699a87f5309430879f3d8161e2cab5d38869562
Chris Haefner <chrismusic1325@gmail.com>
2026-04-19 21:06:54 -0400

    Complete creating arrays practice

---
+++ script.js
@@ -0,0 +1,16 @@
+// Task 1: Create Arrays
+
+let groceryItems = ["Milk", "Bread", "Eggs", "Apples", "Chicken"];
+let prices = [3.99, 2.49, 5.99];
+
+// Task 2: Access and Log the Values
+
+console.log(groceryItems);
+console.log(prices);
+
+console.log(groceryItems[0]);
+console.log(groceryItems[2]);
+console.log(groceryItems[4]);
+
+console.log(prices[0]);
+console.log(prices[2]);
